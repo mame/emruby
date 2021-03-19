a063f64365dbf79d1fc7a8915191a60275cef9da
Yusuke Endoh <mame@ruby-lang.org>
2021-03-19 13:06:12 +0900

    Successfully compiled ruby (MRI) instead of miniruby

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -ex
 git clone https://github.com/ruby/ruby.git --depth 1
 cd ruby
 patch -p1 < ../emscripten.patch
-autoconf
-emconfigure ./configure --host wasm32-unknown-emscripten
-emmake make V=1 miniruby.js EXEEXT=.js
-cp miniruby.js miniruby.wasm ../docs
+./autogen.sh
+emconfigure ./configure --build x86_64-pc-linux-gnu --host wasm32-unknown-emscripten --with-static-linked-ext --with-ext=stringio,ripper,date,strscan,io/console,monitor,digest,pathname,psych
+emmake make
+cp ruby ruby.wasm ../docs
