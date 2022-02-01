bc0af66781bc5b31f7cc7d2999e03209e4493cd8
Yusuke Endoh <mame@ruby-lang.org>
2022-02-01 15:01:36 +0900

    Use LDFLAGS to pass "-sASYNCIFY_STACK_SIZE" option
    ... instead of cflags. Passing it as cflags makes `emcc` emit a warning,
    which confuses the configure script.
    
    Fixes #6

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/ruby/ruby.git --depth 1
 cd ruby
 patch -p1 < ../emscripten.patch
 ./autogen.sh
-./configure --build x86_64-pc-linux-gnu --host wasm32-unknown-emscripten --with-static-linked-ext --with-ext=stringio,ripper,date,strscan,io/console,io/wait,monitor,digest,pathname,psych optflags=-Os debugflags=-g0 CC=emcc LD=emcc AR=emar RANLIB=emranlib cflags="-s ASYNCIFY_STACK_SIZE=65535"
+./configure --build x86_64-pc-linux-gnu --host wasm32-unknown-emscripten --with-static-linked-ext --with-ext=stringio,ripper,date,strscan,io/console,io/wait,monitor,digest,pathname,psych optflags=-Os debugflags=-g0 CC=emcc LD=emcc AR=emar RANLIB=emranlib LDFLAGS="-sASYNCIFY_STACK_SIZE=65536" cflags="-DNDEBUG"
 make
 cp ruby ../app/public/ruby.js
 cp ruby.wasm ../app/public/ruby.wasm
